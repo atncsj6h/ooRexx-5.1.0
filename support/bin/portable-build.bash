9d14334af962d1bf6504fd16faeebed3fea43bc2
Enrico Sorichetti <enricosorichetti@mac.com>
2023-02-12 11:13:42 +0100

    fixed a few glitches related to the api samples Makefiles

--- support/bin/portable-build.bash
+++ support/bin/portable-build.bash
@@ -26,7 +26,7 @@ function __yesno() {
 
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-me="build""        "
+me="build        "
 me=${me:0:8}
 hd="@@  ${me} - "
 
@@ -108,25 +108,35 @@ fi
 echo  "${hd}installing 'portable/${project}-${triplet}' "
 CMAKE_MANIFEST_PREFIX=portable cmake -DCMAKE_INSTALL_PREFIX=portable/${project}-${triplet} -P cmake_install.cmake 1>/dev/null
 
+#   --cleanup ./support/bin/portable-cleanup.sh \
 echo  "${hd}creating '${project}-${version}.run' "
-${makeself} --xz --sha256 \
+${makeself} --xz --sha256 --notemp --nooverwrite \
   --tar-quietly \
   --license portable/${project}-${triplet}/share/NOTICES/CPLv1.0.txt \
-  --cleanup ./support/bin/port-cleanup.sh \
-  portable/${project}-${triplet} ooRexx-${version}.run "ooRexx" ./support/bin/port-startup.sh
+  portable/${project}-${triplet} ooRexx-${version}.run "ooRexx" ./bin/portable-startup.sh
+
 RC=$?
 if [ ${RC} -ne 0 ] ; then
   echo  "${hd}makeself rc(${RC})"
   exit 1
 fi
 
-echo  "${hd}creating  the detached signature for '${project}-${version}' "
+echo  "${hd}creating  the sha256 checksum for '${project}-${version}.run' "
+shasum -a 256 -U  ${project}-${version}.run >${project}-${version}.run.asc
+RC=$?
+if [ ${RC} -ne 0 ] ; then
+  echo  "${hd}shasum rc(${RC})"
+  exit 1
+fi
+
+echo  "${hd}creating  the detached signature for '${project}-${version}.run' "
 gpg -sb --passphrase ${passphrase} --batch --pinentry-mode loopback ${project}-${version}.run
 RC=$?
 if [ ${RC} -ne 0 ] ; then
   echo  "${hd}gpg sign rc(${RC})"
   exit 1
 fi
+
 
 
 echo "${hd}at exit"
---
+++ support/bin/portable-startup.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+Usage()
+{
+  exit $1
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function Usage() {
+  exit $1
+}
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function __yesno() {
+  for ((;;)) ; do
+    echo "${1} Enter [y](ENTER) to continue"
+    echo "${1}       [n/q]      to Exit"
+    read z
+    if [ "${z}". == . ] ; then
+        return 0
+    fi
+    case ${z} in
+      y | Y | yes | YES )  return 0
+          ;;
+      *)  return 1
+          ;;
+    esac
+  done
+}
+
+me="startup          "
+hd="@@  ${me:0:8} - "
+
+if [ 1 -eq 0 ] ; then
+  echo  "${hd}at entry"
+  echo  "${hd}pwd   "$(pwd)
+  echo  "${hd}PATH  "$PATH
+  echo  "${hd}\$0    "$0
+  echo  "${hd}SELF  "$(basename $0)
+  echo  "${hd}argc  "$#
+  echo  "${hd}args  "$@
+  echo "@@"
+fi
+
+clear
+echo  ""
+echo  "${hd} ooRexx has been installed into \"$(pwd)\" "
+echo  "${hd} cd to \"$(pwd)/bin\" "
+echo  "${hd} enter the command \"source path.here\" "
+echo  "${hd} it will prepend the current path to the PATH "
+echo  "${hd} should work both for zsh and bash"
+echo  "${hd} nothing else has to be done"
+echo  ""
+echo  "${hd} to check if things work as they should"
+echo  "${hd} cd to one of the api samples and  enter the command \"make\" "
+echo  "${hd} the relevant rexx script shoud run as planned"
+echo  "${hd} a very good test would be the call.example"
+echo  ""
+echo  "${hd} this script is not needed any longer... "
+echo  "${hd} you can \"rm $(pwd)/bin/$(basename $0)\" "
+echo  ""
+echo  "${hd} you might want to update the PATH/path definition in \"~/.zprofile/~/.bash_profile\" "
+echo  "${hd} adding the ooRexx binary dir \"$(pwd)/bin\" "
+echo  ""
+echo  "${hd} enjoy your fresh installation of \"Open Object Rexx\" "
+__yesno "${hd}"
+
+exit
